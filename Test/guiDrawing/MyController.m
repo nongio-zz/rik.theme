25f1937f254b89d3159bcf22cd0bcaf1f73025b7
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2014-01-06 19:33:01 +0100

    default button test

--- Test/guiDrawing/MyController.m
+++ Test/guiDrawing/MyController.m
@@ -22,12 +22,22 @@
 
   NSSegmentedControl * mySegmentControl = [[NSSegmentedControl alloc]
                                initWithFrame: NSMakeRect(20,380,400,20)];
+  NSButton * abutton = [[NSButton alloc] initWithFrame: NSMakeRect (20, 330, 71, 27)];
+  [abutton setBordered: YES];
+  [abutton setTitle:  _(@"OK")];
+  [abutton setImagePosition: NSImageRight]; 
+  [abutton setImage: [NSImage imageNamed: @"common_ret"]];
+  [abutton setAlternateImage: [NSImage imageNamed: @"common_retH"]];
+  [abutton setAutoresizingMask: NSViewMinXMargin];
+  [[window contentView] addSubview: abutton];
 
   [mySegmentControl setSegmentCount:5];
   [mySegmentControl setLabel:@"First" forSegment:0];
   [mySegmentControl setLabel:@"Second" forSegment:1];
+  [mySegmentControl setLabel:@"Third" forSegment:2];
   [[window contentView] addSubview: mySegmentControl];
 
+  [window setDefaultButtonCell: [abutton cell]];
 }
 
 @end
